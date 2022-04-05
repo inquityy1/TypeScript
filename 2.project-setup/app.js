bdb8783db5acbe5bbaae076b4197d272078dae0d
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-05 11:52:03 +0200

    Add arrays types

--- 2.project-setup/app.js
+++ 2.project-setup/app.js
@@ -1,5 +1,16 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
 var person = {
     name: "Nebojsa",
-    age: 21
+    age: 21,
+    hobbies: ["Sports", "Cooking"]
 };
+var favoriteActivitie;
+favoriteActivitie = ["Sports"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- 2.project-setup/app.ts
+++ 2.project-setup/app.ts
@@ -1,10 +1,19 @@
-const person: {
-  name: string;
-  age: number;
-} = {
-  // const person = {
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
   name: "Nebojsa",
   age: 21,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivitie: string[];
+favoriteActivitie = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()) // ERROR you cant use map on strings
+}
