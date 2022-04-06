66bb9ebab66b0fbf5d64e543efa4086793cb0240
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-06 09:56:20 +0200

    Explained var, const and let

---
+++ 4.next-generation-ts/src/app.ts
@@ -0,0 +1,18 @@
+const userName = "Neca";
+let age = 30;
+
+age = 29;
+
+function add(a: number, b: number) {
+  let result;
+  result = a + b;
+  return result;
+}
+
+if (age > 20) {
+  let isOld = true;
+}
+
+console.log(isOld);
+
+// console.log(result);
