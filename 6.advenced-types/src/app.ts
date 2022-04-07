fe47210173108f830601198211ba98c578e98a02
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-07 09:52:20 +0200

    Add type casting

--- 6.advenced-types/src/app.ts
+++ 6.advenced-types/src/app.ts
@@ -101,3 +101,13 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: "bird", flyingSpeed: 200 });
+
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById("user-input")!
+// );
+
+const userInputElement = document.getElementById("user-input");
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = "Hi there!";
+}
