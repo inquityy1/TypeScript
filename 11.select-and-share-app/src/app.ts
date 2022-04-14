fbcb60d367dc2d280e0ad16787df454eafeb17d6
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-14 21:30:59 +0200

    Add axios for fetching

--- 11.select-and-share-app/src/app.ts
+++ 11.select-and-share-app/src/app.ts
@@ -1,11 +1,19 @@
+import axios from "axios";
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
   // send this to google's API
+  axios.get<GoogleGeocodingResponse>(`your api from google`);
 }
 
 form.addEventListener("submit", searchAddressHandler);
