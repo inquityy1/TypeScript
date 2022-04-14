d27096bfff88a961224a54db24f22905e4f1536e
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-14 13:13:02 +0200

    Add entry and output configuration

--- 9.typescript-practise/src/state/project-state.ts
+++ 9.typescript-practise/src/state/project-state.ts
@@ -1,4 +1,4 @@
-import { Project, ProjectStatus } from "../models/project.js";
+import { Project, ProjectStatus } from "../models/project";
 
 //Project state managment
 type Listener<T> = (items: T[]) => void;
