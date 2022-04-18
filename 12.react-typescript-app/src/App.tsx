df08fd3cefdd40e33607f963e89925166920fef9
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-18 13:27:03 +0200

    Manage state better

--- 12.react-typescript-app/src/App.tsx
+++ 12.react-typescript-app/src/App.tsx
@@ -7,7 +7,10 @@ function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHanlder = (text: string) => {
-    setTodos([{ id: Math.random().toString(), text: text }]);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
   };
 
   return (
