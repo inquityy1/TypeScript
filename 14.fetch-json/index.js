450498748b41d0294c8f4193a6c8176c2f92182f
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-20 13:27:10 +0200

    One quick change

--- 14.fetch-json/index.js
+++ 14.fetch-json/index.js
@@ -3,5 +3,9 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = "https://jsonplaceholder.typicode.com/todos/1";
 axios_1["default"].get(url).then(function (response) {
-  console.log(response.data);
+    var todo = response.data;
+    var ID = todo.ID;
+    var title = todo.Title;
+    var finished = todo.finished;
+    console.log("\n    The Todo with ID: ".concat(ID, "\n    Has a title of: ").concat(title, "\n    Is it finished? ").concat(finished, "\n  "));
 });
