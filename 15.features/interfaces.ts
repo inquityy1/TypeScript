1294f8d8dc33d06e9cb221dddabe4bf7fb1ee92d
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-20 17:55:05 +0200

    Code reuse with interfaces

--- 15.features/interfaces.ts
+++ 15.features/interfaces.ts
@@ -11,8 +11,18 @@ const oldCivic = {
   },
 };
 
+const drink = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  },
+};
+
 const printSummary = (vehicle: Reportable): void => {
   console.log(vehicle.summary());
 };
 
 printSummary(oldCivic);
+printSummary(drink);
