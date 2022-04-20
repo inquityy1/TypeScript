4707e582dab43ede04bfe017eef64295b35fc0dd
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-20 13:35:27 +0200

    Catche errors with TS

--- 14.fetch-json/index.js
+++ 14.fetch-json/index.js
@@ -4,8 +4,8 @@ var axios_1 = require("axios");
 var url = "https://jsonplaceholder.typicode.com/todos/1";
 axios_1["default"].get(url).then(function (response) {
     var todo = response.data;
-    var ID = todo.ID;
-    var title = todo.Title;
-    var finished = todo.finished;
-    console.log("\n    The Todo with ID: ".concat(ID, "\n    Has a title of: ").concat(title, "\n    Is it finished? ").concat(finished, "\n  "));
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    console.log("\n    The Todo with ID: ".concat(id, "\n    Has a title of: ").concat(title, "\n    Is it finished? ").concat(completed, "\n  "));
 });
