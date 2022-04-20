d0044a88116db9bf0ac189d8e0eb834fe13ea2fd
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-20 15:24:11 +0200

    Void and never

--- 15.features/annotations/functions.ts
+++ 15.features/annotations/functions.ts
@@ -5,3 +5,21 @@ const add = (a: number, b: number) => {
 const subtract = (a: number, b: number): number => {
   return a - b;
 };
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
