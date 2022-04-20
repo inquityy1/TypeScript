63051d65a1bc23b94875cdde16387489038b0aa4
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-20 17:45:01 +0200

    Syntax around interfaces

--- 15.features/interfaces.ts
+++ 15.features/interfaces.ts
@@ -1,17 +1,21 @@
+interface Vehicle {
+  name: string;
+  year: Date;
+  broken: boolean;
+  summary(): string;
+}
+
 const oldCivic = {
   name: "civic",
-  year: 2000,
+  year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
-const printVehicle = (vehicle: {
-  name: string;
-  year: number;
-  broken: boolean;
-}): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken: ${vehicle.broken}`);
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(vehicle.summary());
 };
 
 printVehicle(oldCivic);
