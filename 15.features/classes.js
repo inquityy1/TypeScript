3296baa57ef34b7dc150c05afcb3cc73bc4b777e
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-21 14:42:47 +0200

    big update and new project

--- 15.features/classes.js
+++ 15.features/classes.js
@@ -1,14 +1,22 @@
 var Vehicle = /** @class */ (function () {
-    function Vehicle() {
+    function Vehicle(color) {
+        this.color = color;
     }
-    Vehicle.prototype.drive = function () {
-        console.log("chugga chugga");
-    };
     Vehicle.prototype.honk = function () {
         console.log("beep beep i'm a ship i said beepbeep i'm a ship");
     };
     return Vehicle;
 }());
-var vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+var vehicle = new Vehicle("orange");
+console.log(vehicle.color);
+// class Car extends Vehicle {
+//   private drive(): void {
+//     console.log("vroom");
+//   }
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
+// const car = new Car();
+// car.startDrivingProcess();
--- 15.features/classes.ts
+++ 15.features/classes.ts
@@ -1,13 +1,28 @@
 class Vehicle {
-  drive(): void {
-    console.log("chugga chugga");
-  }
+  constructor(public color: string) {}
 
-  honk(): void {
+  protected honk(): void {
     console.log("beep beep i'm a ship i said beepbeep i'm a ship");
   }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
+
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
+  private drive(): void {
+    console.log("vroom");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car(4, "red");
+car.startDrivingProcess();
