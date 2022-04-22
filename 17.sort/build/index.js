318604e051c9d9edd0e9df4ffd3e3ed06eb24bdb
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-22 13:32:33 +0200

    Extract key logic

--- 17.sort/build/index.js
+++ 17.sort/build/index.js
@@ -1,34 +1,4 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection instanceof LinkedList) {
-                    //
-                }
-                // all of this only works if collection is number[]
-                // if collection is an array of numbers
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        var leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                // only going to work if collection is a string
-                // if collection is a string, do this logic instead:
-                // ~~logic to compare and swap characters in a string
-                if (typeof this.collection === "string") {
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
 var sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
 console.log(sorter.collection);
