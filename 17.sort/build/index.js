47b5cf229027a4d3a0555ba4d7873d78f86002a5
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-22 12:31:19 +0200

    Sorter scaffolding

--- 17.sort/build/index.js
+++ 17.sort/build/index.js
@@ -1,6 +1,11 @@
 "use strict";
-console.log("hi there!");
-var logSomething = function () {
-    console.log("something!");
-};
-logSomething();
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () { };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
