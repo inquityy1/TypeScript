cb025b5af84899d09b14516833277af97ea107e7
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-25 14:42:13 +0200

    Describe a row with a tuple

--- 18.stats/src/index.ts
+++ 18.stats/src/index.ts
@@ -1,9 +1,10 @@
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
 const reader = new CsvFileReader("football.csv");
 reader.read();
 
-console.log(reader.data);
+const dateOfFirstMatch = reader.data[0][0];
 
 let manUnitedWins = 0;
 
