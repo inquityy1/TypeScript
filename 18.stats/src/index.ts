351e730ab84368ef5ba238c44a28b372fff45b46
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-26 12:41:54 +0200

    More generics

--- 18.stats/src/index.ts
+++ 18.stats/src/index.ts
@@ -2,7 +2,7 @@ import { MatchReader } from "./MatchReader";
 import { Summary } from "./Summary";
 
 const matchReader = MatchReader.fromCsv("football.csv");
-const summary = Summary.winsAnalysisWithHtmlReport("Man Sity");
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
 
 matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
