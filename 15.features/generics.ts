9149ef4cdefa2dd83e3e5566534a03aba596fb2e
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-04-27 12:23:31 +0200

    New web project

--- 15.features/generics.ts
+++ 15.features/generics.ts
@@ -45,3 +45,30 @@ function printAnything<T>(arr: T[]): void {
 }
 
 printAnything<string>(["a", "b", "c"]);
+
+// Generic Constrains
+
+class Car {
+  print() {
+    console.log("I am a car");
+  }
+}
+
+class House {
+  print() {
+    console.log("I am a house");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()]);
+printHousesOrCars<Car>([new Car(), new Car()]);
