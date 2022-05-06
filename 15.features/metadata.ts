71769ed6d088506990779617aec0a8abadf93837
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-05-06 19:20:19 +0200

    Practical metadata

--- 15.features/metadata.ts
+++ 15.features/metadata.ts
@@ -1,20 +1,45 @@
 import "reflect-metadata";
 
-const plane = {
-  color: "red",
-};
+// const plane = {
+//   color: "red",
+// };
 
-// Reflect.defineMetadata("note", "hi there", plane);
-// Reflect.defineMetadata("height", 10, plane);
+// // Reflect.defineMetadata("note", "hi there", plane);
+// // Reflect.defineMetadata("height", 10, plane);
 
-// const note = Reflect.getMetadata("note", plane);
-// const height = Reflect.getMetadata("height", plane);
+// // const note = Reflect.getMetadata("note", plane);
+// // const height = Reflect.getMetadata("height", plane);
+
+// // console.log(note);
+// // console.log(height);
+
+// Reflect.defineMetadata("note", "hi there", plane, "color");
+
+// const note = Reflect.getMetadata("note", plane, "color");
 
 // console.log(note);
-// console.log(height);
 
-Reflect.defineMetadata("note", "hi there", plane, "color");
+@controller
+class Plane {
+  color: string = "red";
 
-const note = Reflect.getMetadata("note", plane, "color");
+  @get("login")
+  fly(): void {
+    console.log("vrrrrrrrr");
+  }
+}
 
-console.log(note);
+function get(path: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata("path", path, target, key);
+  };
+}
+
+function controller(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const path = Reflect.getMetadata("path", target.prototype, key);
+    const middleware = Reflect.getMetadata("middleware", target.prototype, key);
+
+    router.get(path, middleware, target.prototype[key]);
+  }
+}
