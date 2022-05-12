854a1c204b29ebff846f1f42b75864d97c8b4ae7
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-05-12 18:48:36 +0200

    More on state

--- 22.rts/src/index.tsx
+++ 22.rts/src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
-import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
 
 const App = () => {
   return (
     <div>
-      <GuestList />
+      <UserSearch />
     </div>
   );
 };
