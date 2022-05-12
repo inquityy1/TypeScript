243d82a088c33d9fefcf431d9693c48f8b1a34bc
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-05-12 20:18:55 +0200

    Inline event handlers

--- 22.rts/src/index.tsx
+++ 22.rts/src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
-import UserSearch from "./state/UserSearch";
+import EventComponent from "./events/EventComponent";
 
 const App = () => {
   return (
     <div>
-      <UserSearch />
+      <EventComponent />
     </div>
   );
 };
