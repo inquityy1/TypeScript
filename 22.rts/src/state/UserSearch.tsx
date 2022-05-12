b60054a64b8cdb6085b88c43d99125d6e73743c8
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-05-12 18:59:02 +0200

    Type unions in state

--- 22.rts/src/state/UserSearch.tsx
+++ 22.rts/src/state/UserSearch.tsx
@@ -8,14 +8,23 @@ const users = [
 
 const UserSearch: React.FC = () => {
   const [name, setName] = useState("");
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
 
-  const onClick = () => {};
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+
+    setUser(foundUser);
+  };
 
   return (
     <div>
       User Search
       <input value={name} onChange={(e) => setName(e.target.value)} />
       <button onClick={onClick}>Find User</button>
+      <div>{user && user.name}</div>
+      <div>{user && user.age}</div>
     </div>
   );
 };
