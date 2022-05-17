41e1a73c9ae6efbcf5bc87ec9687980b60807100
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-05-18 01:57:27 +0200

    Correct loader

--- 25.jbook/src/plugins/fetch-plugin.ts
+++ 25.jbook/src/plugins/fetch-plugin.ts
@@ -20,19 +20,20 @@ export const fetchPlugin = (inputCode: string) => {
 
         // Check to see if we alredy fetched this file
         // and if it is in the cache
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
+        // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+        //   args.path
+        // );
 
-        // if it is, return it immediately
-        if (cachedResult) {
-          return cachedResult;
-        }
+        // // if it is, return it immediately
+        // if (cachedResult) {
+        //   return cachedResult;
+        // }
 
         const { data, request } = await axios.get(args.path);
 
+        const loader = args.path.match(/.css$/) ? "css" : "jsx";
         const result: esbuild.OnLoadResult = {
-          loader: "jsx",
+          loader,
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
