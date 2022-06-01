20ab8468d0684ee1115b44b76432193cea8d6685
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-06-01 16:19:32 +0200

    New cell

--- 25.jbook/src/state/action-creators/index.ts
+++ 25.jbook/src/state/action-creators/index.ts
@@ -36,7 +36,7 @@ export const moveCell = (id: string, direction: Direction): MoveCellAction => {
   };
 };
 
-export const isertCellBefore = (
+export const insertCellBefore = (
   id: string,
   cellType: CellTypes
 ): InsertCellBeforeAction => {
