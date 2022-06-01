df62c54eceeb023c624a933331bd282ecb30a18f
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-06-01 16:26:53 +0200

    Display add cell

--- 25.jbook/src/components/cell-list.tsx
+++ 25.jbook/src/components/cell-list.tsx
@@ -1,5 +1,6 @@
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import CellListItem from "./cell-list-item";
+import AddCell from "./add-cell";
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
@@ -7,10 +8,18 @@ const CellList: React.FC = () => {
   );
 
   const renderCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem key={cell.id} cell={cell} />
+    </>
   ));
 
-  return <div>{renderCells}</div>;
+  return (
+    <div>
+      {renderCells}
+      <AddCell nextCellId={null} />
+    </div>
+  );
 };
 
 export default CellList;
