d9ff4bbb54a94ca9b8b7a9998c76097e33959a05
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-06-27 23:59:37 +0200

    Localstorage login and setToken

--- axios-crud-todo-list/src/index.tsx
+++ axios-crud-todo-list/src/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+import App from "./components/App/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
